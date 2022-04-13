4f9afeaae971efc3569feaffdf7c3105891a725a
Yin-Chi Chan <7994343+yinchi@users.noreply.github.com>
2022-04-13 20:43:22 +0100

    Fix render.R to fix stale PDF problem

--- render.R
+++ render.R
@@ -1,4 +1,4 @@
-bookdown::render_book()
+bookdown::render_book(output_format = "all")
 file.rename('movielens.Rmd', 'docs/movielens.Rmd')
 knitr::purl('docs/movielens.Rmd', 'docs/movielens.R')
-file.copy('svd.cpp', 'docs/svd.cpp')
+file.copy('svd.cpp', 'docs/svd.cpp', overwrite = T)
